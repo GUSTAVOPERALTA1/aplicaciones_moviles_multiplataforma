fc2d7cdff968c727f3421f706d4ca275af7efedb
Gustavo Peralta <>
2021-06-01 19:25:20 +0000

    Trabajo completo

---
+++ semana3/for.dart
@@ -0,0 +1,8 @@
+import 'Dart:io';
+
+void main(){
+  print("id_sensor: ");
+  var id= stdin.readLineSync();
+  var lado = int.parse('$id');
+  
+}
--- semana3/sensor.dart
+++ semana3/sensor.dart
@@ -1,6 +1,6 @@
 import "dart:io";
 void main() {
-  print("Hola");
+  print("Hola, bienvenido a nuestro registro");
   final objeto = new Sensores();
   objeto.ingresar();
   objeto.mostrar();
@@ -8,23 +8,28 @@ void main() {
 class Sensores{
   List todo = [];
   void ingresar() {
-    print("Ingrese los datos solicitados: ");
-    print("id_sensor: ");
-    var id= stdin.readLineSync();
-    print("Valor: ");
-    var valor= stdin.readLineSync();
-    print("Fecha del registro: ");
-    var fecha= stdin.readLineSync();
-      
-    Map <dynamic,dynamic> save ={
-   "id sensor": id,
-    "Valor": valor,
-    "Fecha": fecha
-    };
-    todo.add(save);
+    print("¿Cuantos datos desea ingresar?");
+    var a = stdin.readLineSync();
+    var rep = int.parse('$a');
+    for(int i=0; i < rep; i++){
+      print("**********************************************************************");
+      print("Ingrese los datos solicitados: ");
+      print("id_sensor: ");
+      var id= stdin.readLineSync();
+      print("Valor: ");
+      var valor= stdin.readLineSync();
+      print("Fecha del registro: ");
+      var fecha= stdin.readLineSync();      
+      Map <dynamic,dynamic> save ={
+        "id sensor": id,
+        "Valor": valor,
+        "Fecha": fecha
+        };
+        todo.add(save);
+      }
+    }
     
-    
-  }
+  
 
   void mostrar() {
   print("Se muestran todos los datos agregados: ");
